336cc8ee7256508d9f87c7b729ff1b1508af21cb
Kaio Cezar M. G. Araujo <kaio.araujo@gmail.com>
2024-09-25 21:46:50 -0300

    atualização do helpers

--- Main.Module/Application.layer/maintenance-toolkit.cli/src/Helpers/InstallProfiles.mapper.js
+++
@@ -1,8 +0,0 @@
-const INSTALL_PROFILES = {
-    "dev-minimal"  : require("../InstallationProfiles/dev-minimal.install.json"),
-    "dev-standard" : require("../InstallationProfiles/dev-standard.install.json"),
-    "minimal"      : require("../InstallationProfiles/minimal.install.json"),
-    "standard"     : require("../InstallationProfiles/standard.install.json")
-}
-
-module.exports = INSTALL_PROFILES
