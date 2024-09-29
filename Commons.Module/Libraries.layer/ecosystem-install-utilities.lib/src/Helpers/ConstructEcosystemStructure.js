942347a0fa6d4d18d661f2e4c53b3c8df34aa496
Kaio Cezar <kadisk.shark@gmail.com>
2024-09-29 12:42:31 -0300

    ConstructEcosystemStructure

--- Commons.Module/Libraries.layer/ecosystem-install-utilities.lib/src/Helpers/ConstructEcosystemStructure.js
+++ Commons.Module/Libraries.layer/ecosystem-install-utilities.lib/src/Helpers/ConstructEcosystemStructure.js
@@ -1,8 +1,22 @@
-
+const SmartRequire = require("../../../smart-require.lib/src/SmartRequire")
+const colors = SmartRequire("colors")
 const { resolve } = require("path")
 const { 
     mkdir
 } = require('node:fs/promises')
+
+const FilterStringsWithPrefix = (array, prefix) => 
+    array.filter(str => str.startsWith(prefix))
+
+const BuildObjectFromPrefix = (object, prefixAttribute) => {
+    const allKeys = Object.keys(object)
+    const filteredKeys = FilterStringsWithPrefix(allKeys, prefixAttribute)
+    const newObject = filteredKeys.reduce((acc, key) => {
+        return {...acc, [key]: object[key]}
+    }, {})
+
+    return newObject
+}
 
 const ConstructEcosystemStructure = async ({
     ECO_DIRPATH_INSTALL_DATA,
@@ -10,34 +24,26 @@ const ConstructEcosystemStructure = async ({
     loggerEmitter
 }) => {
 
-    const {
-        ECOSYSTEMDATA_CONF_DIRNAME_DOWNLOADED_REPOSITORIES,
-        ECOSYSTEMDATA_CONF_DIRNAME_EXECUTION_DATA_DIR,
-        ECOSYSTEMDATA_CONF_DIRNAME_UNIX_SOCKET_DIR,
-        ECOSYSTEMDATA_CONF_DIRNAME_SUPERVISOR_UNIX_SOCKET_DIR,
-        ECOSYSTEMDATA_CONF_DIRNAME_ESSENTIAL_BINARY_DIR,
-        ECOSYSTEMDATA_CONF_DIRNAME_GLOBAL_EXECUTABLES_DIR,
-        ECOSYSTEMDATA_CONF_DIRNAME_CONFIGURATIONS_DIR,
-        ECOSYSTEMDATA_CONF_DIRNAME_NPM_DEPENDENCIES
-    } = ecosystemDefaults
-
+    const ecosystemDefaultsConfDirname = BuildObjectFromPrefix(ecosystemDefaults, "ECOSYSTEMDATA_CONF_DIRNAME_")
 
     try{
         await mkdir(ECO_DIRPATH_INSTALL_DATA, { recursive: true })
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_DOWNLOADED_REPOSITORIES))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_EXECUTION_DATA_DIR))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_UNIX_SOCKET_DIR))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_SUPERVISOR_UNIX_SOCKET_DIR))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_ESSENTIAL_BINARY_DIR))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_GLOBAL_EXECUTABLES_DIR))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_CONFIGURATIONS_DIR))
-        await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, ECOSYSTEMDATA_CONF_DIRNAME_NPM_DEPENDENCIES))
-
         loggerEmitter && loggerEmitter.emit("log", {
             sourceName: "ConstructEcosystemStructure",
             type: "info",
             message: `o diretório de dados do ecosistema criado com sucesso em ${ECO_DIRPATH_INSTALL_DATA}`
         })
+
+        for (const configKey of Object.keys(ecosystemDefaultsConfDirname)) {
+           const dirname = ecosystemDefaultsConfDirname[configKey]
+           await mkdir(resolve(ECO_DIRPATH_INSTALL_DATA, dirname))
+           loggerEmitter && loggerEmitter.emit("log", {
+                sourceName: "ConstructEcosystemStructure",
+                type: "info",
+                message: `configuração ${ colors.bold(configKey)} : o subdiretório ${ colors.bold(dirname)} criado com sucesso!`
+            })
+        }
+
     } catch (e){
         console.log(e)
         loggerEmitter && loggerEmitter.emit("log", {
