c29b9cc5765aacf07dda9bc75ceb8583be023e90
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-08 11:48:11 -0300

    UpdateEcosystemByProfile

--- Commons.Module/Libraries.layer/ecosystem-install-utilities.lib/src/UpdateEcosystemByProfile.js
+++ Commons.Module/Libraries.layer/ecosystem-install-utilities.lib/src/UpdateEcosystemByProfile.js
@@ -1,12 +1,14 @@
 const path = require("path")
-
 const SmartRequire = require("../../smart-require.lib/src/SmartRequire")
 const colors = SmartRequire("colors")
+const FetchInstalledRepositoriesInfo = require("./Helpers/FetchInstalledRepositoriesInfo")
 
 const UpdateEcosystem = require("./Update/UpdateEcosystem")
 const UpdateRepository = require("./Update/UpdateRepository")
 const PrepareContext  = require("./Helpers/PrepareContext")
 const SynchronizeNodejsDependencies = require("./Helpers/SynchronizeNodejsDependencies")
+
+const VerifyIfAllRepositoriesAreRegistered = require("./Helpers/VerifyIfAllRepositoriesAreRegistered")
 
 const UpdateEcosystemByProfile = async ({
     ecosystemDefaults,
@@ -47,17 +49,32 @@ const UpdateEcosystemByProfile = async ({
     })
 
     if(installationProfile?.repositoriesToInstall){
-        const {
-            repositoriesToInstall
-        } = installationProfile
-    
-        for (const repositoryToInstall of repositoriesToInstall) {
-            await UpdateRepository({
-                repositoryToInstall,
-                ECO_DIRPATH_INSTALL_DATA: absolutInstallDataDirPath,
-                ecosystemDefaults,
-                loggerEmitter
-            })
+
+        const { repositoriesToInstall } = installationProfile
+
+        const areAllRepositoriesRegistered = await VerifyIfAllRepositoriesAreRegistered({
+            repositoriesToInstall,
+            installationPath: absolutInstallDataDirPath,
+            REPOS_CONF_FILENAME_REPOS_DATA: ecosystemDefaults.REPOS_CONF_FILENAME_REPOS_DATA
+        })
+
+        if(areAllRepositoriesRegistered) {
+            const {
+                repositoriesToInstall
+            } = installationProfile
+            
+            for (const repositoryToInstall of repositoriesToInstall) {
+                repositoryToInstall.repository.namespace
+
+                await UpdateRepository({
+                    repositoryToInstall,
+                    ECO_DIRPATH_INSTALL_DATA: absolutInstallDataDirPath,
+                    ecosystemDefaults,
+                    loggerEmitter
+                })
+            }
+        } else {
+            throw "Os repositórios instalados não correspondem com o perfil selecionado"
         }
     }
 }
