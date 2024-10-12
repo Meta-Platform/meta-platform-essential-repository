33db08564bac4710c8a66f98dc1e428070f90bd9
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-12 13:41:40 -0300

    update no tool kit

--- Main.Module/Application.layer/maintenance-toolkit.cli/src/Commands/Install.command.js
+++ Main.Module/Application.layer/maintenance-toolkit.cli/src/Commands/Install.command.js
@@ -3,7 +3,6 @@ const path = require("path")
 const Installer = require("../Helpers/Installer")
 
 const InstallCommand = async ({ profile, installationPath }) => {   
-
     const absoluteInstallationPath = installationPath && path.resolve(process.cwd(), installationPath)
     await Installer({ 
         profile, 
