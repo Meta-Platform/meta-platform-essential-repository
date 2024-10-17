8b3f25dfba61184002e7925d4e860baa0fee443a
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-17 04:55:56 -0300

    ListSources

--- Main.Module/Application.layer/repository-manager.cli/src/Commands/ListSources.command.js
+++ Main.Module/Application.layer/repository-manager.cli/src/Commands/ListSources.command.js
@@ -1,8 +1,30 @@
-
+const colors = require("colors")
 const SOURCES = require("../Configs/repository-sources.json")
 
-const ListSourcesCommand = async (startupParams, cmdParams) => {  
-    console.log(SOURCES)
+const ListSourcesCommand = async () => {  
+
+    console.log(`\n${colors.underline.bold("Lista de fontes de repositórios")}\n`)
+
+    Object.entries(SOURCES)
+    .forEach(([repositoryNamespace, sources]) => {
+        
+        console.log(`\t${colors.bold(repositoryNamespace)}`)
+        sources.forEach((source) => {
+
+            const { sourceType } = source
+
+            const renderOutputInfo = sourceType === "LOCAL_FS"
+             ? source.path
+             : sourceType === "GITHUB_RELEASE"
+                ? `${source.repositoryOwner}/${source.repositoryName}`
+                : sourceType === "GOOGLE_DRIVE"
+                    ? source.fileId
+                    : ""
+
+            console.log(`\t\t${colors.italic.inverse(` ${sourceType} `)} ${colors.dim(renderOutputInfo)}`)
+        })
+
+    })
     
 }
 
