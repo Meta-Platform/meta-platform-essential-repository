482500d421c6098a3bad6adcc03b9f7330c88a2c
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-17 04:10:13 -0300

    ListSourcesCommand

---
+++ Main.Module/Application.layer/repository-manager.cli/src/Commands/ListSources.command.js
@@ -0,0 +1,9 @@
+
+const SOURCES = require("../Configs/repository-sources.json")
+
+const ListSourcesCommand = async (startupParams, cmdParams) => {  
+    console.log(SOURCES)
+    
+}
+
+module.exports = ListSourcesCommand
