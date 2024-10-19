e59717a4d988e86db0eb8199d6e12d698b3af9d3
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-19 02:35:06 -0300

    UpdateRepositoryCommand

--- Main.Module/Application.layer/repository-manager.cli/src/Commands/UpdateRepository.command.js
+++ Main.Module/Application.layer/repository-manager.cli/src/Commands/UpdateRepository.command.js
@@ -1,12 +1,12 @@
 const EventEmitter = require('node:events')
 
-const InstallRepository = require("../../../../../Commons.Module/Libraries.layer/ecosystem-install-utilities.lib/src/InstallRepository")
+const UpdateRepository = require("../../../../../Commons.Module/Libraries.layer/ecosystem-install-utilities.lib/src/UpdateRepository")
 const PrintDataLog = require("../../../../../Commons.Module/Libraries.layer/print-data-log.lib/src/PrintDataLog")
 
 const ECOSYSTEM_DEFAULTS = require("../Configs/ecosystem-defaults.json")
 const REPOSITORY_SOURCES = require("../Configs/repository-sources.json")
 
-const InstallRepositoryCommand = async ({ args, startupParams  }) => {
+const UpdateRepositoryCommand = async ({ args, startupParams  }) => {
 
     const { absolutInstallDataDirPath } = startupParams
 
@@ -16,12 +16,12 @@ const InstallRepositoryCommand = async ({ args, startupParams  }) => {
     } = args
     
     const loggerEmitter = new EventEmitter()
-	loggerEmitter.on("log", (dataLog) => PrintDataLog(dataLog, "InstallRepositoryCommand"))
+	loggerEmitter.on("log", (dataLog) => PrintDataLog(dataLog, "UpdateRepositoryCommand"))
 
     const sourcesList = REPOSITORY_SOURCES[repositoryNamespace]
     const sourceData = sourcesList.find((sourceData) => sourceData.sourceType === sourceType)
 
-    await InstallRepository({
+    await UpdateRepository({
         repositoryNamespace,
         sourceData,
         absolutInstallDataDirPath,
@@ -31,4 +31,4 @@ const InstallRepositoryCommand = async ({ args, startupParams  }) => {
     
 }
 
-module.exports = InstallRepositoryCommand
+module.exports = UpdateRepositoryCommand
