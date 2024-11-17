6ff468a79ae2b10a42d195aad4dc3263d791aa4f
Kaio Cezar <kadisk.shark@gmail.com>
2024-11-16 22:10:37 -0300

    melhoria comando repo sources

--- Main.Module/Application.layer/repository-manager.cli/src/Commands/ShowSourceInformation.command.js
+++ Main.Module/Application.layer/repository-manager.cli/src/Commands/ShowSourceInformation.command.js
@@ -3,20 +3,25 @@ const SOURCES = require("../Configs/repository-sources.json")
 
 const ShowSourceInformationCommand = async () => {  
     Object.entries(SOURCES)
-    .forEach(([repositoryNamespace, sources]) => {
-        console.log("\n====================================================================================\n")
-        console.log(`   ${colors.underline.bold(repositoryNamespace)}\n`)
-        sources.forEach((source, index) => {
-            const paramsNameList = Object.keys(source)
-            paramsNameList.forEach((paramName) => {
-                const paramValueRender = paramName !== "sourceType" ? colors.dim(source[paramName]) : colors.bold(source[paramName])
-                console.log(`\t  ${colors.italic(paramName.padEnd(15))} ${colors.bold("->")} ${paramValueRender}`)
+        .forEach(([repositoryNamespace, sources]) => {
+
+            console.log(`${colors.underline.bold(repositoryNamespace)}`)
+
+            sources.forEach((source, index) => {
+                const paramsNameList = Object.keys(source)
+                paramsNameList.forEach((paramName) => {
+
+                    if(paramName === "sourceType" )
+                        console.log(`   ${colors.bold(source[paramName])}`)
+
+                    if(paramName !== "sourceType" )
+                        console.log(`\t${colors.dim(paramName.padEnd(15))} -> ${colors.dim.italic(source[paramName])}`)
+
+                })
             })
-            if(index < sources.length - 1)
-                console.log("\t--------------------------------------------------------------------")
+            console.log("")
+
         })
-        console.log("\n====================================================================================\n\n")
-    })
 }
 
 module.exports = ShowSourceInformationCommand
