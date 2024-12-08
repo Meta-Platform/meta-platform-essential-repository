752d6a452d3a69d9f62ce891e2aece8e4867823e
Kaio Cezar <kadisk.shark@gmail.com>
2024-12-07 22:27:12 -0300

    remove .rtenv

--- Commons.Module/PlatformLibraries.layer/environment-handler.lib/src/GetEnvironmentPath.js
+++ Commons.Module/PlatformLibraries.layer/environment-handler.lib/src/GetEnvironmentPath.js
@@ -1,6 +1,6 @@
 const { join } = require('path')
 
 const GetEnvironmentPath = (environmentName, localPath) =>
-    `${join(localPath, environmentName)}.rtenv`
+    `${join(localPath, environmentName)}`
 
 module.exports = GetEnvironmentPath
